4ac6e5fd302b59a1e0593607600c0ad83067700b
Кайрат Смаков <@itstep.krg>
2022-09-28 21:05:45 +0600

    Second commit

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ def start(message):
         'restful API serving anime from GogoanimeGogoAnime API.\nEnjoy watching you!'
     bot.send_message(message.chat.id, welcome_message, parse_mode='html', reply_markup=markup)    
 
+
 @bot.message_handler(content_types=['text'])
 def get_user_text(message):
     global anime_number
@@ -53,8 +54,10 @@ def get_user_text(message):
     elif message.text == 'favorites list':
         __show_favorit_list(message)    
 
+
 @bot.callback_query_handler(func=lambda call:True)
 def buttons_handler(call):
+    global recent_release_anime_list
     global popular_anime_list
     global episode_list
     global episode_number
@@ -66,14 +69,31 @@ def buttons_handler(call):
     elif call.data == 'next_episodes':
         __show_anime_episode_buttons(call.message, episode_list)
 
+    anime_list = []
+    
     for anime in popular_anime_list:
+        anime_json = {}
+        anime_json["animeId"] = anime["animeId"]
+        anime_json["animeImg"] = anime["animeImg"]
+        anime_json["animeTitle"] = anime["animeTitle"]
+        anime_list.append(anime_json)
+
+    for anime in recent_release_anime_list:
+        anime_json = {}
+        anime_json["animeId"] = anime["animeId"]
+        anime_json["animeImg"] = anime["animeImg"]
+        anime_json["animeTitle"] = anime["animeTitle"]
+        if __check_duplicate(anime_json, anime_list):
+            anime_list.append(anime_json)
+    
+    for anime in anime_list:
         if call.data == f'list_{anime["animeId"]}':
             episode_number = 0
             __show_anime_detail(call.message, anime["animeId"])
 
         if call.data == f'add_{anime["animeId"]}':
-            user_id = call.message.from_user.id
-            user_file = f'user_favorites\{user_id}.txt'
+            username = call.message.chat.username
+            user_file = f'user_favorites\{username}.txt'
 
             user_favorites_list = []
             if os.path.exists(user_file):
@@ -89,11 +109,20 @@ def buttons_handler(call):
             f.close()
 
 
-def __show_anime(message, anime_id, anime_img, anime_title, anime_info, markup):
+def __check_duplicate(anime_json, anime_list):
+    for a in anime_list:
+        if anime_json["animeId"] == a["animeId"]:
+            return False
+    return True
+
+
+def __show_anime(message, anime_id, anime_img, anime_title, anime_caption, markup):
     
     image_file = f'img/{anime_id}.png'
 
-    __show_anime_photo(message, anime_img, image_file, anime_title, markup)
+    __show_anime_photo(message, anime_img, image_file, anime_title)
+    bot.send_message(message.chat.id, anime_caption, reply_markup=markup)
+
 
 def __show_next_button(message, call_caption: str):
     markup_next = types.InlineKeyboardMarkup()
@@ -101,6 +130,7 @@ def __show_next_button(message, call_caption: str):
         types.InlineKeyboardButton('Next', callback_data=call_caption)
     )
     bot.send_message(message.chat.id, 'for continue click on the <b>Next</b>', parse_mode='html', reply_markup=markup_next)
+
 
 def __show_recent_release_list(message):
     global recent_release_anime_list
@@ -136,6 +166,7 @@ def __show_recent_release_list(message):
             __show_next_button(message, 'next_recent_release')
             break   
 
+
 def __show_popular_list(message):
     global popular_anime_list
     global anime_number
@@ -146,23 +177,23 @@ def __show_popular_list(message):
             n += 1
             continue
 
-        try:
-            markup = types.InlineKeyboardMarkup()
-            markup.row(
-                types.InlineKeyboardButton(' 🗒 series list', callback_data=f'list_{anime["animeId"]}'),
-                types.InlineKeyboardButton(' ➕ add to favorite', callback_data=f'add_{anime["animeId"]}')
-            )
-            __show_anime(
-                message, 
-                anime["animeId"], 
-                anime["animeImg"], 
-                anime["animeTitle"], 
-                f'Released date: {anime["releasedDate"]}',
-                markup)
-        except:
-            anime_number = n + 1
-            __show_next_button(message, 'next_popular')
-            break
+        # try:
+        markup = types.InlineKeyboardMarkup()
+        markup.row(
+            types.InlineKeyboardButton(' 🗒 series list', callback_data=f'list_{anime["animeId"]}'),
+            types.InlineKeyboardButton(' ➕ add to favorite', callback_data=f'add_{anime["animeId"]}')
+        )
+        __show_anime(
+            message, 
+            anime["animeId"], 
+            anime["animeImg"], 
+            anime["animeTitle"], 
+            f'Released date: {anime["releasedDate"]}',
+            markup)
+        # except:
+        #     anime_number = n + 1
+        #     __show_next_button(message, 'next_popular')
+        #     break
         
         n += 1
 
@@ -170,6 +201,7 @@ def __show_popular_list(message):
             anime_number = n - 1
             __show_next_button(message, 'next_popular')
             break   
+
 
 def __show_anime_episode_buttons(message, episode_list):
     global episode_number
@@ -183,7 +215,7 @@ def __show_anime_episode_buttons(message, episode_list):
             continue
 
         button = types.InlineKeyboardButton(
-            f'_{episode["episodeNum"]}_', 
+            f'Episode:{episode["episodeNum"]}', 
             callback_data=f'detail_{episode["episodeId"]}',
             url=episode["episodeUrl"])
 
@@ -191,12 +223,16 @@ def __show_anime_episode_buttons(message, episode_list):
 
         n += 1
 
-        if n > episode_number and n % EPISODE_LIST_PORTION == 0:
-            episode_number = n - 1
-            markup.row(*buttons)
-            bot.send_message(message.chat.id, 'Last episodes: ', reply_markup=markup)    
-            __show_next_button(message, 'next_episodes')
+        if n % EPISODE_LIST_PORTION == 0:
             break
+    
+    episode_number = n
+    markup.row(buttons[0], buttons[1], buttons[2], buttons[3])
+    markup.row(buttons[4], buttons[5], buttons[6], buttons[7])
+    bot.send_message(message.chat.id, 'Last episodes: ', reply_markup=markup)     
+    if episode_number < len(episode_list) - 1:
+        __show_next_button(message, 'next_episodes')
+
 
 def __show_anime_detail(message, anime_id):
     global episode_list
@@ -214,7 +250,8 @@ def __show_anime_detail(message, anime_id):
     episode_number = -1
     __show_anime_episode_buttons(message, episode_list)
 
-def __show_anime_photo(message, image_url, image_file, image_caption, markup=None):
+
+def __show_anime_photo(message, image_url, image_file, image_title):
     r = requests.get(image_url)
 
     if not os.path.exists(image_file):
@@ -223,16 +260,13 @@ def __show_anime_photo(message, image_url, image_file, image_caption, markup=Non
             f.write(r.content)
 
     with open(image_file, 'rb') as f:
-        if markup == None:
-            bot.send_photo(message.chat.id, photo=f, caption=image_caption)
-        else:
-            bot.send_photo(message.chat.id, photo=f, caption=image_caption, reply_markup=markup)    
+        bot.send_photo(message.chat.id, photo=f, caption=image_title)
 
 def __show_favorit_list(message):
     global favorit_number
 
-    user_id = message.from_user.id
-    user_file = f'user_favorites\{user_id}.txt'
+    username = message.from_user.username
+    user_file = f'user_favorites\{username}.txt'
 
     user_favorites_list = []
     if os.path.exists(user_file):
@@ -240,6 +274,10 @@ def __show_favorit_list(message):
         json_content = f.read()
         user_favorites_list = json.loads(json_content)
         f.close()
+
+    if len(user_favorites_list) == 0:
+        bot.send_message(message.chat.id, 'Favorites list is empty.')
+        return
 
     n = 0
 
@@ -259,7 +297,7 @@ def __show_favorit_list(message):
                 anime["animeId"], 
                 anime["animeImg"], 
                 anime["animeTitle"], 
-                f'Released date: {anime["releasedDate"]}',
+                f' ',
                 markup)
         except:
             favorit_number = n + 1
@@ -272,5 +310,6 @@ def __show_favorit_list(message):
             favorit_number = n - 1
             __show_next_button(message, 'next_favorit')
             break   
+
 
 bot.infinity_polling()
